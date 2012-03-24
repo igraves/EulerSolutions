de1a07d42b1b25da3d25f33d8d74676b1f187678
Ian Graves <thegravian@gmail.com>
2012-03-23 23:40:02 -0500

    Problem 3

--- Problem3/Euler3.hs
+++ Problem3/Euler3.hs
@@ -1,11 +1,15 @@
 module Euler3 where
 
+res n = maximum $ primefactors n
 
+primefactors n = case takeNext nums of
+                        Nothing -> [n]
+                        Just n@(n',rem) -> n' : primefactors rem
+  where
+         nums = [2..(n `div` 2)]
+         takeNext [] = Nothing
+         takeNext (x:xs) = if n `mod` x == 0
+                              then Just $ (x,n `div` x)
+                              else takeNext xs
 
-
-factors n = filter (\x -> n `mod` x == 0) nums  
-	where
-    	   nums = [2..(n `div` 2)]
-
-intSqrt = floor . sqrt . fromIntegral
 
